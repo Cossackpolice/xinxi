fa18dc2f1f9e0afa659d1071288da6065ab23896
Your Name <you@example.com>
2019-12-12 19:03:49 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/student_info.java
@@ -0,0 +1,30 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class student_info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_info);
+        Intent intent = getIntent();
+        Student student = (Student) intent.getSerializableExtra(TableContanst.STUDENT_TABLE);
+        ((TextView)findViewById(R.id.tv_info_id)).setText(student.getId()+"");
+        ((TextView)findViewById(R.id.tv_info_name)).setText(student.getName());
+        ((TextView)findViewById(R.id.tv_info_age)).setText(student.getAge()+"");
+        ((TextView)findViewById(R.id.tv_info_sex)).setText(student.getSex());
+        ((TextView)findViewById(R.id.tv_info_likes)).setText(student.getLike());
+        ((TextView)findViewById(R.id.tv_info_train_date)).setText(student.getTrainDate());
+        ((TextView)findViewById(R.id.tv_info_phone)).setText(student.getPhoneNumber());
+    }
+    public void goBack(View view) {
+        finish();
+    }
+}
